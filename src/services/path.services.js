2054b9404f9569614229cbac75e786ee1b26124c
roberto reyes <robertostanleyreyes@gmail.com>
2022-05-19 15:27:43 -0600

    se termino el flujo de rutas

--- src/services/path.services.js
+++ src/services/path.services.js
@@ -1,13 +1,14 @@
 import {http_client} from "@/plugins/http_client";
-const url='/api/v1/rutas'
+
+const url = '/api/v1/rutas'
 const getPaths = async (params = {}) => await http_client(url, params, 'get')
-const storePaths = async (body = {}) => await http_client(url, body, 'post')
-const getPath = async (params = {}) => await http_client(url, params, 'get')
-const updatePath = async (params = {}) => await http_client(url, params, 'get')
+const storePath = async (body) => await http_client(url, body, 'post')
+const getPath = async (idPath) => await http_client(`${url}/${idPath}`)
+const updatePath = async (idPath, body) => await http_client(`${url}/${idPath}`, body, 'put')
 
 export default {
     getPaths,
-    storePaths,
+    storePath,
     getPath,
     updatePath
 }
--- src/views/rutas/formPath.vue
+++ src/views/rutas/formPath.vue
@@ -8,44 +8,47 @@
         >
           Rutas
         </v-card-title>
-        <v-row>
-          <v-col cols="12" md="6">
-            <v-card-text>
+        <v-card-text>
+          <v-row>
+            <v-col cols="12" md="6">
+
               <v-text-field
-                  label="Nombre"
-                  color="blueMinsal"
-                  v-model="form.nombre"
+                  label="Nombre" @blur="$v.form.name.$touch()"
+                  color="blueMinsal" :error-messages="nameErrors"
+                  v-model="form.name"
               ></v-text-field>
               <v-text-field
-                  label="URI"
-                  color="blueMinsal"
-                  v-model="form.uri"
+                  label="URI" @blur="$v.form.url.$touch()"
+                  color="blueMinsal" :error-messages="urlErrors"
+                  v-model="form.url"
               ></v-text-field>
               <v-text-field
-                  label="Nombre URI"
-                  color="blueMinsal"
-                  v-model="form.nombre_uri"
+                  label="Nombre URI" @blur="$v.form.name_url.$touch()"
+                  color="blueMinsal" :error-messages="urlNameErrors"
+                  v-model="form.name_url"
               ></v-text-field>
               <v-text-field
                   label="Icono"
-                  :append-icon="form.icono"
+                  :append-icon="form.icon"
                   color="blueMinsal"
-                  v-model="form.icono"
+                  v-model="form.icon"
               ></v-text-field>
               <v-text-field
                   label="Orden"
                   color="blueMinsal"
-                  v-model="form.orden"
+                  v-model.number="form.order"
               ></v-text-field>
+              <v-autocomplete v-model="form.id_ruta_padre" :items="paths" item-text="nombre" item-value="id"
+                              label="Ruta Padre"></v-autocomplete>
               <v-checkbox
                   label="Mostrar"
-                  v-model="form.mostrar"
+                  v-model="form.show"
                   class="d-inline-flex"
                   color="blueMinsal"
               ></v-checkbox>
               <v-checkbox
                   label="Publico"
-                  v-model="form.publico"
+                  v-model="form.public"
                   class="d-inline-flex ml-2"
                   color="blueMinsal"
               ></v-checkbox>
@@ -55,24 +58,13 @@
                   class="d-inline-flex ml-2"
                   color="blueMinsal"
               ></v-checkbox>
-            </v-card-text>
-          </v-col>
-          <v-col cols="12" md="6">
-            <v-card-text>
-              <v-select
-                  :items="roles"
-                  v-model="form.roles"
-                  label="Seleccione roles"
-                  multiple
-                  item-text="name"
-                  item-value="id"
-                  chips
-                  color="blueMinsal"
-                  item-color="blueMinsal"
-              ></v-select>
-            </v-card-text>
-          </v-col>
-        </v-row>
+            </v-col>
+            <v-col cols="12" md="6">
+              <app-search-list :items="roles" v-model="form.roles" item-value="id" item-text="name"></app-search-list>
+            </v-col>
+          </v-row>
+        </v-card-text>
+
         <v-card-actions>
           <v-row class="pa-0 ma-0">
             <v-col class="pa-0">
@@ -82,17 +74,18 @@
                   rounded
                   @click="savePath(true)"
                   :small="$vuetify.breakpoint.xs"
+                  :loading="loading_navigate"
               >
                 <v-icon left>mdi-content-save</v-icon>
-                Crear y regresar al listado
+                {{ !$route.params.id ? 'Crear' : 'Editar' }} y regresar al listado
               </v-btn
               >
-              <v-btn
-                  color="blueMinsal"
-                  class="white--text ma-1"
-                  :small="$vuetify.breakpoint.xs"
-                  rounded
-                  @click="savePath()"
+              <v-btn v-if="!$route.params.id"
+                     color="blueMinsal"
+                     class="white--text ma-1"
+                     :small="$vuetify.breakpoint.xs"
+                     rounded :loading="loading"
+                     @click="savePath()"
               >
                 <v-icon left>mdi-content-save-all</v-icon>
                 Crear y agregar otro
@@ -109,36 +102,152 @@
 </template>
 <script>
 import {mapActions} from "vuex";
-import paths from "../../services/path.services";
+import AppSearchList from "../../components/AppSearchList";
+import {required} from "vuelidate/lib/validators";
 
 export default {
   name: "formPath",
+  components: {AppSearchList},
+  validations: {
+    form: {
+      name: {
+        required,
+      },
+      url: {
+        required
+      },
+      name_url: {
+        required
+      }
+    },
+  },
   data: () => ({
     form: {
-      nombre: null,
-      uri: null,
-      nombre_uri: null,
-      icono: null,
-      mostrar: false,
-      publico: false,
-      orden: null,
+      name: null,
+      url: null,
+      name_url: null,
+      icon: null,
+      show: false,
+      public: false,
+      order: null,
       admin: false,
+      id_ruta_padre: null,
       roles: []
     },
     roles: [],
+    paths: [],
+    loading: false,
+    loading_navigate: false
   }),
   methods: {
     ...mapActions("utils", ["getMenu"]),
+    formClean() {
+      this.$v.$reset()
+      this.form.public = false
+      this.form.show = false
+      this.form.icon = null
+      this.form.order = null
+      this.form.name = null
+      this.form.admin = null
+      this.form.url = null
+      this.form.name_url = null
+      this.form.id_ruta_padre = null
+      this.form.roles = []
+    },
     async getRoles() {
-      const response = await this.services.paths.getPaths()
+      const response = await this.services.roles.getRoles()
       this.roles = response.data;
     },
+    async getPath() {
+      const response = await this.services.paths.getPath(this.$route.params.id)
+      const {data} = response
+      this.form.name = data.nombre
+      this.form.url = data.uri
+      this.form.name_url = data.nombre_uri
+      this.form.icon = data.icono
+      this.form.order = data.orden
+      this.form.id_ruta_padre = data.id_ruta_padre
+      this.form.show = data.mostrar
+      this.form.public = data.publico
+      this.form.admin = data.admin
+      this.form.roles = data.Rols.map(rol => rol.id)
+    },
     async savePath(navigate = false) {
+      this.$v.$touch()
+      if (!this.$v.$invalid) {
+        try {
+          this.stringEmptyToNull()
+          if (navigate) this.loading_navigate = true
+          else this.loading = true
 
+          const body = {
+            nombre: this.form.name,
+            uri: this.form.url,
+            nombre_uri: this.form.name_url,
+            mostrar: this.form.show,
+            icono: this.form.icon,
+            orden: this.form.order,
+            admin: this.form.admin,
+            publico: this.form.public,
+            id_ruta_padre: this.form.id_ruta_padre,
+            roles: this.form.roles
+          }
+
+          if (this.$route.params.id)
+            await this.services.paths.updatePath(this.$route.params.id, body)
+          else
+            await this.services.paths.storePath(body)
+          this.temporalAlert({
+            show: true,
+            type: 'success',
+            message: this.$route.params.id ? 'Actualizado' : 'Registrado'
+          })
+          this.formClean()
+          if (navigate)
+            await this.$router.push({name: 'rutasList'})
+        } catch {
+        } finally {
+          this.loading = false
+          this.loading_navigate = false
+        }
+      }
+    },
+    async getPaths() {
+      const response = await this.services.paths.getPaths()
+      this.paths = response.data
+    },
+    stringEmptyToNull() {
+      for (const properties in this.form) {
+        if (typeof this.form[properties] === 'string' && this.form[properties] === '')
+          this.form[properties] = null
+      }
+    }
+  },
+  computed: {
+    nameErrors() {
+      const errors = []
+      if (!this.$v.form.name.$dirty) return errors
+      !this.$v.form.name.required && errors.push('Nombre es obligatorio')
+      return errors
+    },
+    urlErrors() {
+      const errors = []
+      if (!this.$v.form.url.$dirty) return errors
+      !this.$v.form.url.required && errors.push('URI es obligatoria')
+      return errors
+    },
+    urlNameErrors() {
+      const errors = []
+      if (!this.$v.form.name_url.$dirty) return errors
+      !this.$v.form.name_url.required && errors.push('Nombre URI es obligatoria')
+      return errors
     },
   },
   async created() {
-    await this.getRoles();
+    if (this.$route.params.id) {
+      this.getPath()
+    }
+    await Promise.race([this.getPaths(), this.getRoles()])
   },
 };
 </script>
