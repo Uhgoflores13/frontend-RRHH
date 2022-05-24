8219945477633da652ad4da6030236ff33c5e60f
roberto reyes <robertostanleyreyes@gmail.com>
2022-05-24 12:00:43 -0600

    se agrego el menu de mas de un nivel

--- src/components/AppAsideBar.vue
+++ src/components/AppAsideBar.vue
@@ -1,55 +1,78 @@
 <template>
   <v-navigation-drawer
-    app
-    permanent
-    stateless
-    :class="sideBar ? 'px-0' : 'px-2'"
-    :mini-variant="sideBar"
-    mini-variant-width="70"
-    @input="setSibeBar($event)"
+      app
+      permanent
+      stateless
+      :class="sideBar ? 'px-0' : 'px-2'"
+      :mini-variant="sideBar"
+      mini-variant-width="70"
+      @input="setSibeBar($event)"
   >
     <template v-slot:prepend>
-      <v-card elevation="1" class="ma-4 py-0" v-if="!sideBar">
-        <v-card-title class="pa-0">
+      <v-card elevation="2" class="ma-4 py-0" v-if="!sideBar">
+        <v-card-title>
           <img
-            :src="require(`@/assets/img/SIS-HAZUL.png`)"
-            width="95%"
-            height="auto"
+              :src="require(`@/assets/img/SIS-HAZUL.png`)"
+              width="95%"
+              height="auto"
           />
         </v-card-title>
       </v-card>
       <v-list-item v-else two-line>
-        <v-btn text icon x-large color="blueMinsal" 
-          ><v-icon size="30">mdi-asterisk</v-icon></v-btn
+        <v-btn text icon x-large color="blueMinsal"
+        >
+          <v-icon size="30">mdi-asterisk</v-icon>
+        </v-btn
         >
       </v-list-item>
     </template>
-    <v-list dense nav>
-      <v-list-item
-        v-for="(item, i) in menu"
-        :key="i"
-        :to="item.uri"
-        active-class=" blueMinsal--text"
-      >
-        <v-tooltip right :disabled="!sideBar" color="blueMinsal">
-          <template v-slot:activator="{ on, attrs }">
-            <v-list-item-icon v-bind="attrs" v-on="on">
-              <v-icon>{{ item.icono }}</v-icon>
-            </v-list-item-icon>
-          </template>
-          <span>{{ item.nombre }}</span>
-        </v-tooltip>
 
-        <v-list-item-title class="text-capitalize">{{
-          item.nombre
-        }}</v-list-item-title>
-      </v-list-item>
+    <v-list dense nav>
+      <span v-for="(item, i) in menu"
+            :key="i">
+
+        <v-list-group active-class="blueMinsal--text" v-if="item.childrens && item.childrens.length">
+        <template v-slot:activator>
+          <v-list-item-title class="text-capitalize">
+            {{ item.nombre }}
+          </v-list-item-title>
+        </template>
+        <template #prependIcon>
+          <v-icon color="blueMinsal">{{ item.icono }}</v-icon>
+        </template>
+          <div>
+            <v-list-item v-for="(child, j) in item.childrens" :key="j" :to="child.uri" active-class="blueMinsal--text">
+              <v-list-item-icon>
+                <v-icon>{{ child.icono }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title class="text-capitalize">
+                <span>{{ child.nombre }}</span>
+              </v-list-item-title>
+            </v-list-item>
+          </div>
+      </v-list-group>
+        <v-list-item v-else :to="item.uri"
+                     active-class=" blueMinsal--text">
+          <v-tooltip right :disabled="!sideBar" color="blueMinsal">
+            <template v-slot:activator="{ on, attrs }">
+              <v-list-item-icon v-bind="attrs" v-on="on">
+                <v-icon>{{ item.icono }}</v-icon>
+              </v-list-item-icon>
+            </template>
+            <span>{{ item.nombre }}</span>
+          </v-tooltip>
+
+        <v-list-item-title class="text-capitalize">
+          {{ item.nombre }}
+        </v-list-item-title>
+        </v-list-item>
+      </span>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
-import { mapMutations, mapState } from "vuex";
+import {mapMutations, mapState} from "vuex";
 
 export default {
   name: "AppAsideBar",
@@ -57,11 +80,10 @@ export default {
     menu: {
       type: Array,
       default: () => {
-        return [{ uri: "/", nombre: "Inicio", icono: "mdi-home" }];
+        return [];
       },
     },
   },
-  data: () => ({}),
   methods: {
     ...mapMutations("utils", ["setSibeBar"]),
   },
